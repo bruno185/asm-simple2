d0dd844aceb57099c04c4f2a630618a24e5b981e
bruno185 <37832013+bruno185@users.noreply.github.com>
2020-08-20 18:02:39 +0200

    print

--- asm-simple2/m.asm
+++ asm-simple2/m.asm
@@ -1,22 +1,48 @@
-.386
-.model flat, stdcall
-.stack 4096
-ExitProcess PROTO, dwExitCode: DWORD
+EXTRN GetStdHandle: PROC
+EXTRN WriteFile:    PROC
+EXTRN lstrlen:      PROC
+EXTRN ExitProcess:  PROC
 
-.data
+STD_OUTPUT = -11
 
-	; variables here
+.DATA
 
-.code
+    hFile        QWORD 0
+    msglen       DWORD 0
+    BytesWritten DWORD 0
+    msg          BYTE  "Hello x64 World!", 13, 10, 0
+    
 
-main PROC
+.CODE
 
-	; code here
-	mov eax,1
-	mov ebx,2
-	add eax,ebx
-	sal eax,1
-	INVOKE ExitProcess, 0
+    main PROC
 
-main ENDP
-END main
+	;int 3              ; breakpoint for debugger
+
+        sub rsp, 28h    ; obligatoire, mais pourquoi (pas obligatoire si sortie avec call ExitProcess) 
+        ;space for 4 arguments + 16byte aligned stack
+        ;https://stackoverflow.com/questions/19128291/stack-alignment-in-x64-assembly
+
+        lea rcx, msg
+        call lstrlen
+        mov msglen, eax
+
+	mov ecx, STD_OUTPUT        ; STD_OUTPUT
+	call GetStdHandle
+        mov hFile, rax
+
+        lea r9, BytesWritten
+       	mov r8d, msglen
+	lea rdx, msg
+        mov rcx, hFile
+	call WriteFile
+
+	
+    ;xor ecx, ecx        ; exit code = 0
+	;call ExitProcess
+    add rsp,28h
+    ret
+
+    main ENDP
+
+END
